ea02b4083f5f9662952113fce8d9444d11c7a38d
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2018-12-01 14:44:02 -0500

    tire stiffness

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -124,8 +124,8 @@ class CarInterface(object):
     centerToFront_civic = wheelbase_civic * 0.4
     centerToRear_civic = wheelbase_civic - centerToFront_civic
     rotationalInertia_civic = 2500
-    tireStiffnessFront_civic = 128100
-    tireStiffnessRear_civic = 135500
+    tireStiffnessFront_civic = 85400
+    tireStiffnessRear_civic = 90000
 
     centerToRear = ret.wheelbase - ret.centerToFront
     # TODO: get actual value, for now starting with reasonable value for
