d1e8bffb3380ba0d1a8d8c7a5874e8b8cb9d5297
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2018-12-30 11:57:38 -0500

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -259,7 +259,7 @@ class CarInterface(object):
     ret.seatbeltUnlatched = not self.CS.seatbelt
     ret.gearShifter = self.CS.gear_shifter
     #ret.readdistancelines = self.CS.follow_level
-    ret.distanceToggle = self.CS.follow_level
+    #ret.distanceToggle = self.CS.follow_level
 
     buttonEvents = []
 
