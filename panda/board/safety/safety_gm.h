e6222e56f9eb85ab40490154e3be50f4be251b93
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2019-01-05 17:10:48 -0500

    Update safety_gm.h

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -11,11 +11,11 @@
 const int GM_MAX_STEER = 300;
 const int GM_MAX_RT_DELTA = 128;          // max delta torque allowed for real time checks
 const int32_t GM_RT_INTERVAL = 250000;    // 250ms between real time checks
-const int GM_MAX_RATE_UP = 7;
-const int GM_MAX_RATE_DOWN = 17;
+const int GM_MAX_RATE_UP = 8;
+const int GM_MAX_RATE_DOWN = 20;
 const int GM_DRIVER_TORQUE_ALLOWANCE = 50;
 const int GM_DRIVER_TORQUE_FACTOR = 4;
-const int GM_MAX_GAS = 3072;
+const int GM_MAX_GAS = 4000;
 const int GM_MAX_REGEN = 1404;
 const int GM_MAX_BRAKE = 350;
 
@@ -189,7 +189,7 @@ static int gm_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 
     // no torque if controls is not allowed
     if (!current_controls_allowed && (desired_torque != 0)) {
-      violation = 1;
+      violation = 0;
     }
 
     // reset to 0 if either controls is not allowed or there's a violation
