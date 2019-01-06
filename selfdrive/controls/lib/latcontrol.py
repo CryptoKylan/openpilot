844d0e5a5a415e829b5aa0f1de0aaf17049b9852
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2019-01-06 16:56:25 -0500

    Update latcontrol.py

--- selfdrive/controls/lib/latcontrol.py
+++ selfdrive/controls/lib/latcontrol.py
@@ -35,8 +35,8 @@ class LatControl(object):
   def __init__(self, CP):
 
     _ADJUST_REACTANCE = 1.2
-    _ADJUST_INDUCTANCE = 1.2
-    _ADJUST_RESISTANCE = 1.0
+    _ADJUST_INDUCTANCE = 1.5
+    _ADJUST_RESISTANCE = 2.0
 
     KpV = [np.interp(25.0, CP.steerKpBP, CP.steerKpV) * _ADJUST_REACTANCE]
     KiV = [np.interp(25.0, CP.steerKiBP, CP.steerKiV) * _ADJUST_REACTANCE]
