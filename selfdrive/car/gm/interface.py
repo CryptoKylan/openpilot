a8914575f00bf1e0c9cef0276e6ac8030cca2d37
Kylan <20389320+CryptoKylan@users.noreply.github.com>
2019-01-11 13:15:18 -0500

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -189,7 +189,7 @@ class CarInterface(object):
       ret.steerReactance = 1.0
       ret.steerInductance = 1.0
       ret.steerResistance = 1.0
-      ret.eonToFront = 0.5
+      ret.eonToFront = 1.0
 
       ret.steerMaxBP = [0.] # m/s
       ret.steerMaxV = [1.]
